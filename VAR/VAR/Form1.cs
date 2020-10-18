5a2b7fcba26c9bc493658083fdda574d0056288d
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-10-18 19:07:04 +0200

    Portfólió érték kiszámítása

--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -74,5 +74,20 @@ y in Portfolio
 
             dataGridView2.DataSource = Portfolio;
         }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
