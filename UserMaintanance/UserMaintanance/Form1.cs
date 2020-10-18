6f5c5ae951d17d5844a73d3ae9ec36a3a66f98ed
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-10-18 18:08:49 +0200

    Button

--- UserMaintanance/UserMaintanance/Form1.cs
+++ UserMaintanance/UserMaintanance/Form1.cs
@@ -7,17 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using UserMaintanance.Entities;
 namespace UserMaintanance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource.LatName; // label1
             lblFirstName.Text = Resource.FirstName; // label2
             btnAdd.Text = Resource.Add; // button1
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
