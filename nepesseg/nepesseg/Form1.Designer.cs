9703d7f56b7e61f677c25b94ec54989344ce16e9
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-11-08 16:54:43 +0100

    8

--- nepesseg/nepesseg/Form1.Designer.cs
+++ nepesseg/nepesseg/Form1.Designer.cs
@@ -28,60 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.csvpathtxt = new System.Windows.Forms.TextBox();
+            this.browsebtn = new System.Windows.Forms.Button();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.zaroev = new System.Windows.Forms.NumericUpDown();
+            this.eredmenytxt = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.zaroev)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // csvpathtxt
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.csvpathtxt.Location = new System.Drawing.Point(262, 8);
+            this.csvpathtxt.Margin = new System.Windows.Forms.Padding(2);
+            this.csvpathtxt.Multiline = true;
+            this.csvpathtxt.Name = "csvpathtxt";
+            this.csvpathtxt.Size = new System.Drawing.Size(123, 56);
+            this.csvpathtxt.TabIndex = 13;
             // 
-            // dataGridView2
+            // browsebtn
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(277, 12);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView2.TabIndex = 1;
+            this.browsebtn.Location = new System.Drawing.Point(389, 10);
+            this.browsebtn.Margin = new System.Windows.Forms.Padding(2);
+            this.browsebtn.Name = "browsebtn";
+            this.browsebtn.Size = new System.Drawing.Size(93, 54);
+            this.browsebtn.TabIndex = 12;
+            this.browsebtn.Text = "Browse";
+            this.browsebtn.UseVisualStyleBackColor = true;
+            this.browsebtn.Click += new System.EventHandler(this.browsebtn_Click);
             // 
-            // dataGridView3
+            // startbtn
             // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(548, 12);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView3.TabIndex = 2;
+            this.startbtn.Location = new System.Drawing.Point(487, 8);
+            this.startbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(95, 55);
+            this.startbtn.TabIndex = 10;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(178, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Population file";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(60, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "End year";
+            // 
+            // zaroev
+            // 
+            this.zaroev.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.zaroev.Location = new System.Drawing.Point(11, 45);
+            this.zaroev.Margin = new System.Windows.Forms.Padding(2);
+            this.zaroev.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.zaroev.Name = "zaroev";
+            this.zaroev.Size = new System.Drawing.Size(154, 19);
+            this.zaroev.TabIndex = 7;
+            this.zaroev.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.zaroev.Value = new decimal(new int[] {
+            2025,
+            0,
+            0,
+            0});
+            // 
+            // eredmenytxt
+            // 
+            this.eredmenytxt.Location = new System.Drawing.Point(11, 69);
+            this.eredmenytxt.Name = "eredmenytxt";
+            this.eredmenytxt.Size = new System.Drawing.Size(571, 344);
+            this.eredmenytxt.TabIndex = 14;
+            this.eredmenytxt.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView3);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(612, 425);
+            this.Controls.Add(this.eredmenytxt);
+            this.Controls.Add(this.csvpathtxt);
+            this.Controls.Add(this.browsebtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zaroev);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zaroev)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox csvpathtxt;
+        private System.Windows.Forms.Button browsebtn;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown zaroev;
+        private System.Windows.Forms.RichTextBox eredmenytxt;
     }
 }
 
--- nepesseg/nepesseg/Form1.cs
+++ nepesseg/nepesseg/Form1.cs
@@ -18,25 +18,29 @@ namespace nepesseg
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
-        Random rng = new Random(1234);
+        Random rng = new Random();
 
         public Form1()
         {
             InitializeComponent();
 
+            csvpathtxt.Text = "E:\\Documents\\IRF\\VersionControl\\nép-teszt.csv";
             Population = GetPopulation(@"E:\Documents\IRF\VersionControl\nép-teszt.csv");
-          
+
             BirthProbabilities = GetBirthProbalities(@"E:\Documents\IRF\VersionControl\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"E:\Documents\IRF\VersionControl\halál.csv");
-            dataGridView1.DataSource = Population;
-            dataGridView2.DataSource = BirthProbabilities;
-            dataGridView3.DataSource = DeathProbabilities;
+          
 
-            for (int year = 2005; year <= 2024; year++)
+            
+        }
+
+        private void simulate(int zaroev)
+        {
+            for (int year = 2005; year <= zaroev; year++)
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -45,8 +49,45 @@ namespace nepesseg
                 int nbrOfFemales = (from x in Population
                                     where x.Gender == Gender.Female && x.IsAlive
                                     select x).Count();
-                Console.WriteLine(
-                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+                /*Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));*/
+                eredmenytxt.Text = eredmenytxt.Text + string.Format("Szimulációs év:{0}" + Environment.NewLine + "\t" + " Fiuk:{1}" + Environment.NewLine + "\t" + " Lányok:{2}" + Environment.NewLine, year, nbrOfMales, nbrOfFemales);
+
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+
+            if (!person.IsAlive) return;
+
+
+            byte age = (byte)(year - person.BirthYear);
+
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
             }
         }
 
@@ -107,6 +148,28 @@ namespace nepesseg
                 }
             }
             return Population;
+        }
+
+        private void browsebtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = false;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                csvpathtxt.Text = choofdlog.FileName.ToString();
+
+            }
+            Population = GetPopulation(choofdlog.FileName);
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            eredmenytxt.Text = "";
+            simulate(int.Parse(zaroev.Value.ToString()));
+            
         }
     }
 }
