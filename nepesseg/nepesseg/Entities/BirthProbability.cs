d47f28d83c64d20beee4e0907a1bba5c3e2c5d4d
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-11-08 16:23:48 +0100

    1.

---
+++ nepesseg/nepesseg/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg.Entities
+{
+    class BirthProbability
+    {
+       // kor, gyermekek száma és a valószínűség
+
+             public int Age { get; set; }
+             public byte NumberOfChilden { get; set; }
+             public double P { get; set; }
+    }
+}
---
+++ nepesseg/nepesseg/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg.Entities
+{
+    class DeathProbability
+    {
+
+        //nem, kor és a halálozási valószínűség
+        public Gender gender { get; set; }
+        public int Age { get; set; }
+        public double P { get; set; }
+    }
+}
