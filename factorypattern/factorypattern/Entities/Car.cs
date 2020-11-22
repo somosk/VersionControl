cfba2b27b4f4260d1acf0e65535a44e756ce2651
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-11-22 19:08:20 +0100

    7

---
+++ factorypattern/factorypattern/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using factorypattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorypattern.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
