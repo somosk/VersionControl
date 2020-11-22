9336cf846507e61479b91d81e1805a42494af66e
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-11-22 19:05:14 +0100

    6

--- factorypattern/factorypattern/Entities/Ball.cs
+++ factorypattern/factorypattern/Entities/Ball.cs
@@ -8,29 +8,11 @@ using System.Windows.Forms;
 
 namespace factorypattern.Entities
 {
-    public class Ball : Label
-            {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+    public class Ball : Toy
+    {
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
