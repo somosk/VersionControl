6c219aa53327f35a90539b49569c2efb45b523fa
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-12-06 17:22:41 +0100

    7

--- WorldHG/WorldHG/Form1.cs
+++ WorldHG/WorldHG/Form1.cs
@@ -20,6 +20,8 @@ namespace WorldHG
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -45,6 +47,17 @@ namespace WorldHG
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
