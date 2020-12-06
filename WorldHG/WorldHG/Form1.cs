2f517ca307db0b0b6c500b24148ade16aeba4fff
Somos Krisztián <krisztian.somos3@stud.uni-corvinus.hu>
2020-12-06 17:16:37 +0100

    4

--- WorldHG/WorldHG/Form1.cs
+++ WorldHG/WorldHG/Form1.cs
@@ -15,11 +15,24 @@ namespace WorldHG
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            // gc.AddPlayer();
+            //gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
